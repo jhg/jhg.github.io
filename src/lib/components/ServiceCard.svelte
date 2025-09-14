c0839dc411e05e840c92a84275618b9a6fea4248
Jesus Hernandez <jesushdez@protonmail.com>
2025-09-14 17:37:46 +0200

    Refactor of services grid to use a card component

---
+++ src/lib/components/ServiceCard.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { children } = $props();
+</script>
+
+<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
+	{@render children?.()}
+</div>
--- src/lib/components/ServicesGrid.svelte
+++ src/lib/components/ServicesGrid.svelte
@@ -1,26 +1,27 @@
 <script>
+import ServiceCard from "$lib/components/ServiceCard.svelte";
 </script>
 
 <div class="text-center max-w-3xl mx-auto">
 	<h3 class="text-xl font-semibold text-white mb-4 text-stroke">Services</h3>
 	<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3.5 md:gap-4 lg:gap-4.5 text-gray-200">
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            Digital Product<br>Development
-        </div>
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            Web<br>Applications
-        </div>
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            Technology<br>Strategy
-        </div>
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            Mobile & Desktop<br>Apps
-        </div>
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            AI<br>Integrations
-        </div>
-		<div class="p-4 rounded border border-gray-400/25 min-h-32 flex items-center justify-center bg-glass shadow-glass">
-            Servers<br>High-Performance
-        </div>
+		<ServiceCard>
+			Digital Product<br>Development
+		</ServiceCard>
+		<ServiceCard>
+			Web<br>Applications
+		</ServiceCard>
+		<ServiceCard>
+			Technology<br>Strategy
+		</ServiceCard>
+		<ServiceCard>
+			Mobile & Desktop<br>Apps
+		</ServiceCard>
+		<ServiceCard>
+			AI<br>Integrations
+		</ServiceCard>
+		<ServiceCard>
+			Servers<br>High-Performance
+		</ServiceCard>
 	</div>
 </div>
