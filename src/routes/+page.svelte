8f333a0b200f9a3e79b27a249d7c884e5bf8fd41
Jesus Hernandez <jesushdez@protonmail.com>
2025-09-14 15:29:42 +0200

    Reorder services

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -71,12 +71,12 @@
 	<div class="text-center max-w-3xl mx-auto">
 		<h3 class="text-xl font-semibold text-white mb-4 text-stroke">Services</h3>
 		<div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-3.5 md:gap-4 lg:gap-4.5 text-gray-200">
-			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Digital Product Development</div>
+			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Digital Product<br>Development</div>
 			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Webs</div>
-            <div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Technology Strategy</div>
-			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Mobile / Desktop Apps</div>
-            <div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">LLM API integrations</div>
-            <div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">MCP servers</div>
+			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Technology<br>Strategy</div>
+			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">Mobile / Desktop<br>Apps</div>
+			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">MCP servers</div>
+			<div class="p-4 rounded border border-gray-400/25 min-h-30 flex items-center justify-center bg-glass shadow-glass">AI API<br>integrations</div>
 		</div>
 	</div>
 </section>
