c657346e8339eb2f4fcae06328c1567fa2451599
Jesus Hernandez <jesushdez@protonmail.com>
2025-09-15 01:16:15 +0200

    Refactor of social media links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,7 @@
 	import ContactForm from '$lib/components/ContactForm.svelte';
 	import heroImage from '$lib/assets/img/wb-jesus-hernandez.avif';
 	import ServicesGrid from "$lib/components/ServicesGrid.svelte";
-	import GithubIcon from '$lib/assets/img/social/github.svg?component';
-	import DiscordIcon from '$lib/assets/img/social/discord.svg?component';
-	import linkedinIcon from '$lib/assets/img/social/linkedin.png';
+	import SocialLinks from "$lib/components/SocialLinks.svelte";
 </script>
 
 <svelte:head>
@@ -102,15 +100,5 @@
     <h2 id="social-heading" class="text-xl font-semibold text-white mb-6 text-stroke xl:landscape:hidden">
         Connect with me
     </h2>
-    <div class="flex justify-center gap-6 bg-glass shadow-glass rounded-lg px-4 py-4 mx-auto w-fit xl:landscape:fixed xl:landscape:right-6 xl:landscape:top-1/2 xl:landscape:translate-y-16 xl:landscape:flex-col xl:landscape:gap-4 xl:landscape:pt-6 xl:landscape:pl-4">
-        <a href="https://linkedin.com/in/jesushdezhere" class="social-link group" aria-label="LinkedIn Profile" rel="noopener noreferrer" target="_blank">
-            <img src={linkedinIcon} alt="LinkedIn" width="840" height="779" class="md:landscape:w-10 md:landscape:h-auto w-auto h-10 opacity-70 group-hover:opacity-100 transition-opacity duration-300" />
-        </a>
-        <a href="https://github.com/jhg" class="social-link group" aria-label="GitHub Profile" rel="noopener noreferrer" target="_blank">
-            <GithubIcon class="w-10 h-10 fill-white opacity-70 group-hover:opacity-100 transition-opacity duration-300" />
-        </a>
-        <a href="https://discord.gg/9xW8CDx87d" class="social-link group" aria-label="Discord Server" rel="nofollow noopener noreferrer" target="_blank">
-            <DiscordIcon class="w-10 h-10 fill-white opacity-70 group-hover:opacity-100 transition-opacity duration-300" />
-        </a>
-    </div>
+    <SocialLinks />
 </section>
